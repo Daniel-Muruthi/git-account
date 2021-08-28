0c56f1c2c5a50147280c7491b86bea7d1b18d0fd
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-08-28 13:56:28 +0300

    This will add the username input field

---
+++ src/app/inputform/inputform.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputform',
+  templateUrl: './inputform.component.html',
+  styleUrls: ['./inputform.component.css']
+})
+export class InputformComponent implements OnInit {
+
+  username=""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  gitName(event: Event){
+    this.username=(<HTMLInputElement>event.target).value
+  }
+
+}
